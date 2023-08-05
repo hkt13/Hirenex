6a0493422442270f711f6cbbe2128bdfacdbdf92
Harsh <thakurharsh1112@gmail.com>
2023-08-05 10:30:02 +0530

    image path updated

--- src/components/HappyCustomers.jsx
+++ src/components/HappyCustomers.jsx
@@ -1,11 +1,12 @@
 import '../style/HappyCustomers.css'
+import '../../images/companies.png'
 const HappyCustomers=()=>{
 
     return(
         <div className="hero-customers">
              <section className="happycustomers">
             <h1>leading companies build teams on hirenex</h1>
-            <img src="/images/companies.png" alt="" className="companies-img"/>
+            <img src= '../../images/companies.png' alt="" className="companies-img"/>
         </section>
         </div>
        
